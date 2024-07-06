d32fe72d66f40bc7420ba61b396aa9517373724b
RogerCasas22 <roger_1997_xd@hotmail.com>
2024-07-06 01:51:12 -0500

    clase servicio CRUD para Producto

---
+++ src/main/java/pe/com/cibertec/service/ProductoService.java
@@ -0,0 +1,14 @@
+package pe.com.cibertec.service;
+
+import java.util.Optional;
+
+import pe.com.cibertec.model.Producto;
+
+public interface ProductoService {
+	
+	public Producto save( Producto producto);
+	public Optional<Producto> get(Integer id);
+	public void update(Producto producto);
+	public void delete(Integer id);
+
+}
---
+++ src/main/java/pe/com/cibertec/service/ProductoServiceImpl.java
@@ -0,0 +1,37 @@
+package pe.com.cibertec.service;
+
+import java.util.Optional;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import pe.com.cibertec.model.Producto;
+import pe.com.cibertec.repository.ProductoRepository;
+
+@Service
+public class ProductoServiceImpl implements ProductoService {
+
+	@Autowired
+	private ProductoRepository productoRepository;
+
+	@Override
+	public Producto save(Producto producto) {
+		return productoRepository.save(producto);
+	}
+
+	@Override
+	public Optional<Producto> get(Integer id) {
+		return productoRepository.findById(id);
+	}
+
+	@Override
+	public void update(Producto producto) {
+		productoRepository.save(producto);		
+	}
+
+	@Override
+	public void delete(Integer id) {
+		productoRepository.deleteById(id);		
+	}
+
+}
