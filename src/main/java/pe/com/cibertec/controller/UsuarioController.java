266f05761dd58d2063db7a0331624e78795ab4e2
RogerCasas22 <roger_1997_xd@hotmail.com>
2024-07-07 09:15:20 -0500

    Autenticación de usuario temporal

--- src/main/java/pe/com/cibertec/controller/UsuarioController.java
+++ src/main/java/pe/com/cibertec/controller/UsuarioController.java
@@ -15,6 +15,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import jakarta.servlet.http.HttpSession;
 import pe.com.cibertec.model.Usuario;
 import pe.com.cibertec.service.IUsuarioService;
 
@@ -35,11 +36,41 @@ public class UsuarioController {
 			return "usuario/registro";
 		}
 		
+		// guardar ususario en la base de datos
 		@PostMapping("/save")
 		public String save(Usuario usuario) {
 			logger.info("Usuario registro: {}", usuario);
 			usuario.setTipo("USER");
 			usuarioService.save(usuario);		
+			return "redirect:/";
+		}
+		
+		//login
+		@GetMapping("/login")
+		public String login() {
+			return "usuario/login";
+		}
+		
+		//acceder con usuario temporal
+		@PostMapping("/acceder")
+		public String acceder(Usuario usuario, HttpSession session) {
+			logger.info("Accesos : {}", usuario);
+			
+			Optional<Usuario> user=usuarioService.findByEmail(usuario.getEmail());
+			//logger.info("Usuario de db: {}", user.get());
+			
+			if (user.isPresent()) {
+				session.setAttribute("idusuario", user.get().getId());
+				
+				if (user.get().getTipo().equals("ADMIN")) {
+					return "redirect:/administrador";
+				}else {
+					return "redirect:/";
+				}
+			}else {
+				logger.info("Usuario no existe");
+			}
+			
 			return "redirect:/";
 		}
 }
