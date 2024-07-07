edc68977d48bb037f91bfab7fad811efa41d7e45
RogerCasas22 <roger_1997_xd@hotmail.com>
2024-07-07 02:24:37 -0500

    mostrar los productos en la vista home de admin

--- src/main/java/pe/com/cibertec/controller/AdministradorController.java
+++ src/main/java/pe/com/cibertec/controller/AdministradorController.java
@@ -1,15 +1,31 @@
 package pe.com.cibertec.controller;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+
+import pe.com.cibertec.model.Producto;
+import pe.com.cibertec.service.ProductoService;
 
 @Controller
 @RequestMapping("/administrador")
 public class AdministradorController {
 	
+	@Autowired
+	private ProductoService productoService;
+	
+	//mostar los productos en la vista home del administrador
 	@GetMapping("")
-	public String home(){
+	public String home(Model model) {
+
+		List<Producto> productos = productoService.findAll();
+		model.addAttribute("productos", productos);
+
+
 		return "administrador/home";
 	}
 
